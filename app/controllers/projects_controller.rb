4cf88184855d6a9a876cb69a32508c99c87122b1
Magesh <magesh@geexhq.com>
2011-12-27 22:13:08 +0530

    Project - created a simple page to add and remove projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,26 @@
 class ProjectsController < ApplicationController
+  
+  def index
+    @users = User.all
+    @projects = Project.all
+  end
+
+  def create
+    @project=Project.new(params[:project])
+    if @project.save
+      redirect_to :action => 'index'
+      else
+      render 'index'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      redirect_to :action => 'index'
+      else
+      redirect_to :back
+    end
+  end
+
 end
