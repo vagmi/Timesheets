1734b92ac8e80a0cbf024fc1344dd89b0d2fa730
Magesh <magesh@geexhq.com>
2012-01-01 22:33:55 +0530

    added links and routes for statistics and timesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def get_monthname(mon)
+    month_arr=%w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
+    for i in 0..month_arr.length-1
+      return month_arr[i] if mon-1==i
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
 Timesheets::Application.routes.draw do
-  resources :projects
+  resources :projects do
+    get :autocomplete_project_name, :on => :collection
+    post :project_graph, :on => :collection
+    get :show_my_statistics, :on => :collection
+    get :my_graph, :on => :collection
+  end
+  
   resources :categories
   resources :time_entries
+  resources :timesheets do 
+    post 'load_timesheet', :on => :collection
+  end
+  
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
     get 'logout' => 'devise/sessions#destroy'
   end
 
   root :to=>"home#index"
+  
+  match '/projects/project_graph/:id' => 'projects#project_graph'
+  match '/projects/static_graph/:id' => 'projects#static_graph'
+  match '/projects/dynamic_graph/:id' => 'projects#dynamic_graph'
+  match '/projects/my_graph/:id' => 'projects#my_graph'
 end
